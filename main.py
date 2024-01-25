c97f9159f3a39d57ffd3dba2fc7a64329df33f33
HyHyZhaLee <huy.ly2053038@hcmut.edu.vn>
2024-01-25 15:43:46 +0700

    Upload all

---
+++ main.py
@@ -0,0 +1,19 @@
+from adafruit_mqtt import *
+import random
+import time
+feed_ids = ["button1", 	"button2", "humidity", "light", "temperature"]
+username = "AI_ProjectHGL"
+password = "aio_" + "CHRX08OBUahjbuHBFWdtmdIVwibh"
+
+mqtt = Adafruit_MQTT(username, password, feed_ids)
+
+while True:
+    time.sleep(3)
+    print("Publishing...")
+    mqtt.publish("button1",random.randint(0,1))
+    mqtt.publish("button2",random.randint(0,1))
+    mqtt.publish("humidity",random.randint(0,100))
+    mqtt.publish("temperature",random.randint(0,100))
+    mqtt.publish("light",random.randint(0,100))
+    pass
+
